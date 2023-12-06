8e8d1ddc966359f45f4f4c6c707abb087d4bf6de
alon bensa <alonbensa@gmail.com>
2023-12-06 11:17:14 +0200

    update frontend

--- Frontend-React-Project/src/pages/UserProfilePage/UserProfile.jsx
+++ Frontend-React-Project/src/pages/UserProfilePage/UserProfile.jsx
@@ -1,37 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../../components/NavbarComp/Navbar";
 
-function UserProfile() {
-  const [userData, setUserData] = useState(null);
-
-  const fetchUserInformation = async (username) => {
-    try {
-      const response = await fetch(`https://api.example.com/users/${username}`);
-      const userData = await response.json();
-      setUserData(userData);
-      setIsLoading(false);
-    } catch (error) {
-      setError("Error fetching user information");
-      setIsLoading(false);
-      console.error("Error fetching user information:", error);
-    }
-  };
-
-  useEffect(() => {
-    const username = useParams().username;
-    if (username) {
-      fetchUserInformation(username);
-    }
-  }, []);
-
+function UserProfile({username, description, profileImage, userFollower, userFollowing, PostsNumber, genres, songs}) {
   return (
     <>
       <Navbar></Navbar>
       <div className="userProfile">
-        <img src={userData.profilePicture} alt="Profile Picture" />
-        <h2>{userData.name}</h2>
-        <p>Email: {userData.email}</p>
-        <p>Bio: {userData.bio}</p>
+        <img></img>
+        <h1></h1>
+        <div className="follow-infos">
+          <h3 className="follower"></h3>
+          <h3 className="following"></h3>
+          <h3 className="posts"></h3>
+        </div>
+        <p></p>
+        <div className="genres">
+          {genres.map((genre, index))}
+        </div>
+        <div className="my-songs">
+          {songs.map((song, index))}
+        </div>
       </div>
     </>
   );
