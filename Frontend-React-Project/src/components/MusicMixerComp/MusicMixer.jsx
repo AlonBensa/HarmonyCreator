b29600370ae97f5f285c301b90a832b97dd2582a
alonBensa <alonbensa@gmail.com>
2023-12-06 09:42:17 +0200

    no message

--- Frontend-React-Project/src/components/MusicMixerComp/MusicMixer.jsx
+++ Frontend-React-Project/src/components/MusicMixerComp/MusicMixer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import "./MusicMixer.css";
 import Note from "../NoteComp/Note";
@@ -6,39 +6,69 @@ import Note from "../NoteComp/Note";
 function MusicMixer({ sequences, setSequences, maxAtLine, initializeLines }) {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentColumn, setCurrentColumn] = useState(0);
-  const [highlightedNotes, setHighlightedNotes] = useState([]);
+  const [audioSrc, setAudioSrc] = useState(null);
+  const audioRef = useRef();
+  const noteTime = 900;
 
   useEffect(() => {
     let intervalId;
-  
-    const playNotes = () => {
-      // Logic to play notes in the current column
-      console.log(`Playing notes in column ${currentColumn}`);
-  
+
+    const playNotes = async () => {
+      clearInterval(intervalId);
+
       // Create an array to store highlighted notes for the current column
       const columnNotes = [];
-  
+
       // Iterate through each line and add the note at the current column to the highlighted notes array
       sequences.forEach((sequence) => {
         columnNotes.push(sequence[currentColumn]);
       });
-  
-      // Highlight the notes in the current column
-      setHighlightedNotes(columnNotes);
-  
+
+      // Create an array to store promises for each note
+      const playPromises = [];
+
+      columnNotes.forEach((note) => {
+        if (note !== " ") {
+          const audioPath = `../../assets/pianoNotes/${note}.mp3`;
+
+          const playPromise = new Promise((resolve) => {
+            import(/* @vite-ignore */ audioPath)
+              .then((audioModule) => {
+                const audio = new Audio(audioModule.default);
+                audio.addEventListener("ended", resolve);
+
+                // Play for 1 second
+                audio.play();
+                setTimeout(() => {
+                  audio.pause();
+                  audio.currentTime = 0; // Reset the audio to the beginning
+                  resolve(); // Resolve after 1 second
+                }, noteTime);
+              })
+              .catch((error) => {
+                console.error(error);
+                resolve(); // Resolve even if there's an error to continue playing other notes
+              });
+          });
+
+          playPromises.push(playPromise);
+        }
+      });
+
+      // Wait for all notes to finish playing before moving to the next column
+      await Promise.all(playPromises);
+
       // Update the column to highlight the next one
       setCurrentColumn((prevColumn) => (prevColumn + 1) % maxAtLine);
     };
-  
+
     if (isPlaying) {
       // Start playing notes with a delay
-      intervalId = setInterval(playNotes, 1000); // Adjust the interval as needed
-    }
-    else{
+      intervalId = setInterval(playNotes, noteTime); // Adjust the interval as needed
+    } else {
       setCurrentColumn(0);
-      setHighlightedNotes([]);
     }
-  
+
     return () => {
       // Cleanup function to stop interval when the component unmounts or when isPlaying is set to false
       clearInterval(intervalId);
@@ -50,6 +80,27 @@ function MusicMixer({ sequences, setSequences, maxAtLine, initializeLines }) {
     setIsPlaying((prevIsPlaying) => !prevIsPlaying);
   };
 
+  useEffect(() => {
+    if (audioSrc) {
+      audioRef.current = new Audio(audioSrc);
+
+      // Remove the previous audio reference
+      return () => {
+        audioRef.current = null;
+      };
+    }
+  }, [audioSrc]);
+
+  useEffect(() => {
+    // Play the audio when the component renders or when audioSrc changes
+    if (audioRef.current) {
+      audioRef.current.play().then(() => {
+        // After playing, reset the audio source
+        setAudioSrc(null);
+      });
+    }
+  }, [audioSrc]);
+
   return (
     <div className="black-square">
       <button
@@ -59,7 +110,7 @@ function MusicMixer({ sequences, setSequences, maxAtLine, initializeLines }) {
         {isPlaying ? "Stop" : "Play"}
       </button>
       {sequences.map((sequence, sequenceIndex) => (
-         <div className="sequence" key={sequenceIndex}>
+        <div className="sequence" key={sequenceIndex}>
           {sequence.map((note, noteIndex) => (
             <Note
               key={noteIndex}
